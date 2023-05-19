ea1d40c91342485142123e0125a30c52a25b4234
Mauricio Guerrero Gayosso <65859942+MauGayosso@users.noreply.github.com>
2023-05-19 11:18:05 -0600

    New Functions

---
+++ FileExplorer/WindowClientsMenu.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FileExplorer
+{
+    /// <summary>
+    /// Lógica de interacción para WindowClientsMenu.xaml
+    /// </summary>
+    public partial class WindowClientsMenu : Window
+    {
+        public WindowClientsMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnEntrar_Click(object sender, RoutedEventArgs e)
+        {
+            if(comboBox1.Text == null)
+            {
+                MessageBox.Show("Selecciona una opcion para continuar","Advertencia",MessageBoxButton.OK);
+            }
+            else if (comboBox1.Text == "Cliente 1")
+            {
+         
+                MainWindow win = new MainWindow();
+                win.Show();
+                Close();
+            }
+        }
+    }
+}
--- FileExplorer/WindowLogin.xaml.cs
+++ FileExplorer/WindowLogin.xaml.cs
@@ -11,7 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
 namespace FileExplorer
 {
     /// <summary>
@@ -19,9 +23,15 @@ namespace FileExplorer
     /// </summary>
     public partial class WindowLogin : Window
     {
+        string pathDB = "C:/Users/mauri/source/repos/FileExplorer/FileExplorer/FileExplorer/MI_DB/clientes.db";
+        SQLiteCommand command;
+        SQLiteConnection connection = new SQLiteConnection($"Data Source=C:/Users/mauri/source/repos/FileExplorer/FileExplorer/FileExplorer/MI_DB/clientes.db;Version=3");
+        SQLiteDataReader reader;
+
         public WindowLogin()
         {
             InitializeComponent();
+            connection.Open();
         }
 
         private void txtUsuario_TextChanged(object sender, TextChangedEventArgs e)
@@ -31,7 +41,58 @@ namespace FileExplorer
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            string username = txtUsuario.Text;
+            string password = txtPassword.Password;
+            Debug.WriteLine("USERNAME : " + username);
+            Debug.WriteLine("PASSWORD: " + password);
+            VerificarCredenciales(username, password);
 
+
+        }
+
+
+        private void VerificarCredenciales(string username, string password)
+        {
+            string nameMachine = System.Environment.MachineName;
+            string logFilePath = "C:/Users/mauri/OneDrive/Escritorio/log.txt";
+            Debug.WriteLine("Name Machine : " + nameMachine);
+            using (StreamWriter write = File.AppendText(logFilePath))
+            {
+                write.WriteLine($"[{DateTime.Now}, {nameMachine},{username}]");
+            }
+            string query = "SELECT COUNT(*) FROM Usuarios WHERE username=@username AND password=@password";
+            using (SQLiteCommand command = new SQLiteCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", password);
+                int count = Convert.ToInt32(command.ExecuteScalar());
+                
+                if(count > 0) 
+                {
+                    WindowClientsMenu win = new WindowClientsMenu();
+                    win.Show();
+                    Close();
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Usuario y/o Contraseña Invalidos", "Error", MessageBoxButton.OK);
+                }
+
+            }
+        }
+
+        private void btnVisitante_Click(object sender, RoutedEventArgs e)
+        {
+            string nameMachine = System.Environment.MachineName;
+            string logFilePath = "C:/Users/mauri/OneDrive/Escritorio/log.txt";
+            Debug.WriteLine("Name Machine : " + nameMachine);
+            using (StreamWriter write = File.AppendText(logFilePath))
+            {
+                write.WriteLine($"[{DateTime.Now}, {nameMachine}]");
+            }
+            WindowAyudasVisuales win = new WindowAyudasVisuales();
+            win.Show();
+            Close();
         }
     }
 }
